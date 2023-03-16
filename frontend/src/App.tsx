24d7b61a034ce2754fa7c7cc4c9e7f494df48d1d
Miguel Leite <miguelleite200leite@gmal.com>
2023-03-16 16:05:02 +0100

    feat(chat) added feature to talk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
-import { AuthPage } from "./AuthPage";
-import { ChatsPage } from "./ChatsPage";
+import "./App.css";
 
+import {AuthPage} from "./AuthPage";
+import {ChatsPage} from "./ChatsPage";
 
 function App() {
   const [user, setUser] = useState(undefined);
 
   if (!user) {
-    return <AuthPage onAuth={(user:any) => setUser(user)} />
+    return <AuthPage onAuth={(user:any) => setUser(user)} />;
   } else {
-    return <ChatsPage user={user} />
+    return <ChatsPage user={user} />;
   }
 }
 
-export default App
+export default App;
--- frontend/src/ChatsPage.tsx
+++ frontend/src/ChatsPage.tsx
@@ -1,12 +1,17 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 interface ChatsPageProps {
   user: any;
 }
 
-export function ChatsPage({ user }:ChatsPageProps) {
+export function ChatsPage(props:any) {
   return (
-    <div>
-      chats...
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
     </div>
   );
 }
